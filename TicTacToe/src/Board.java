8c83ae904f21403f74653897547123996c2ec96b
tristen.miller <tristen.miller@MU-C1-08.mullen.pvt.k12.co.us>
2025-09-26 12:29:29 -0600

    Created a display board with a matrix.also made a new nested for loop in the prepare board class in put it into the main method.

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
 
 public class Board
 	{
-		String [] [] myBoard = new String [3] [3];
+		static Scanner userStringInput = new Scanner(System.in);
+		static String [] [] myBoard = new String [3] [3];
+		public static void displayBoard()
+		{
+        System.out.println("    1    2    3");
+		System.out.println("  ----------------");
+		System.out.println("A |    |    |    |");
+		System.out.println("  ----------------");
+	    System.out.println("B |    |    |    |");
+		System.out.println("  ----------------");
+		System.out.println("C |    |    |    |");
+		System.out.println("  ----------------");
+		}  
+		
+		
 			
+			public static void prepareBoard()
+				{
+					for (int row = 0; row < myBoard.length; row ++)
+						{
+							for(int col = 0; col <myBoard[0].length;col++)
+								{
+									myBoard[row][col] = " ";
+								}
+						}
+					
+				}
+	
+	
+	
 	}
+
+
+         
+    
 	
--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -6,15 +6,16 @@ public class TicTacToe
 		static String xo;
 		public static void main(String[] args)
 			{
-			welcomeMessage();
-
+			//welcomeMessage();
+              Board.displayBoard();
+              Board.prepareBoard();
 			}
 
-		private static void welcomeMessage()
+		public static void welcomeMessage()
 			{
+				
 				System.out.println("Welcome to Tic Tac Toe!");
 				System.out.println("What would You like to be X or O?");
-				System.out.println("hello Mr.Mcguire, I am at home currently doing nothing with this project because I dont wnat to do this until until tomorrow");
 				xo = userStringInput.nextLine();
 				
 				
