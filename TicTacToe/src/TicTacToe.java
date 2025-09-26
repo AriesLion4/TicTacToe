23ba695e0dad894ca0f3c8488c46e9558e4a7cad
Summi <Summi@DESKTOP-6UC4OQQ>
2025-09-25 21:35:19 -0600

    Changed syso message for homework assignment with push and pull.

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -14,6 +14,7 @@ public class TicTacToe
 			{
 				System.out.println("Welcome to Tic Tac Toe!");
 				System.out.println("What would You like to be X or O?");
+				System.out.println("hello Mr.Mcguire, I am at home currently doing nothing with this project because I dont wnat to do this until until tomorrow");
 				xo = userStringInput.nextLine();
 				
 				
